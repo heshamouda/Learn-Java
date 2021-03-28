2d87a3cb5e299688b5fc2644513003db70a75d2e
heshamouda <hesham.ouda@students.fhnw.ch>
2021-03-28 01:35:24 +0100

    edit class Database

--- personData/src/com/HeshamOuda/model/Database.java
+++ personData/src/com/HeshamOuda/model/Database.java
@@ -1,5 +1,26 @@
 package com.HeshamOuda.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database {
 
+	private List<Person> personData;
+
+	public Database() {
+		super();
+		this.personData = new ArrayList<>();
+	}
+
+	public void addPerson(Person person) {
+		this.personData.add(person);
+	}
+
+	public void removeerson(Person person) {
+		this.personData.remove(person);
+	}
+
+	public List<Person> getPersonData() {
+		return this.personData;
+	}
 }

2da6c0c5ab07a1732679460833c6878534c0f544
heshamouda <hesham.ouda@students.fhnw.ch>
2021-03-28 01:27:17 +0100

    edit class Person

--- personData/src/com/HeshamOuda/model/Person.java
+++ personData/src/com/HeshamOuda/model/Person.java
@@ -1,5 +1,44 @@
 package com.HeshamOuda.model;
 
 public class Person {
+	private String name;
+	private String occupation;
 
+	/**
+	 * @param name
+	 * @param occupation
+	 */
+	public Person(String name, String occupation) {
+		super();
+		this.name = name;
+		this.occupation = occupation;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the occupation
+	 */
+	public String getOccupation() {
+		return occupation;
+	}
+
+	/**
+	 * @param occupation the occupation to set
+	 */
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
 }

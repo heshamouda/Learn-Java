f9b4710f8f4d01357dcac09b0d819be762d8b75d
heshamouda <hesham.ouda@students.fhnw.ch>
2021-03-28 03:17:43 +0200

    edit the class textPanel

--- personData/src/com/HeshamOuda/view/TextPanel.java
+++ personData/src/com/HeshamOuda/view/TextPanel.java
@@ -1,5 +1,37 @@
 package com.HeshamOuda.view;
 
-public class TextPanel {
+import java.awt.BorderLayout;
+import java.awt.Font;
 
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class TextPanel extends JPanel {
+	private static final long SerialVersionUID = 1L;
+	private JTextArea textArea;
+
+	public TextPanel() {
+		initializeClass();
+		setLayout();
+	}
+
+	private void setLayout() {
+		setLayout(new BorderLayout());
+		textArea.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+	}
+
+	private void initializeClass() {
+		this.textArea = new JTextArea();
+	}
+
+	public void addText(String textString) {
+		this.textArea.append(textString + "\n");
+	}
+
+	public void clearText() {
+		this.textArea.setText("");
+	}
 }

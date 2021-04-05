055dbf56369b1b81a599d7e0838a5ddd8475565e
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-04-05 22:29:54 +0200

    add new class jradio-button

--- java_swing/src/javaSwing/Jradiobutton.java
+++ java_swing/src/javaSwing/Jradiobutton.java
@@ -9,34 +9,34 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 
-public class Jradiobutton extends JFrame implements ActionListener{
+public class Jradiobutton extends JFrame implements ActionListener {
 	private JRadioButton r1, r2;
 	JButton bt;
 	private JFrame f = new JFrame("RdioButton exemple");
-	
+
 	public Jradiobutton() {
-		f.setSize(500,500);
+		f.setSize(500, 500);
 		f.setLayout(null);
 		f.setVisible(true);
-		
+
 		r1 = new JRadioButton("Male");
 		r1.setBounds(70, 50, 100, 35);
-		
+
 		r2 = new JRadioButton("Female");
 		r2.setBounds(70, 100, 100, 35);
-		
+
 		bt = new JButton("Cick");
 		bt.setBounds(70, 150, 80, 35);
 		bt.addActionListener(this);
-		
+
 		ButtonGroup g = new ButtonGroup();
 		g.add(r1);
 		g.add(r2);
-		
+
 		f.add(r1);
 		f.add(r2);
 		f.add(bt);
-		
+
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
@@ -47,11 +47,10 @@ public class Jradiobutton extends JFrame implements ActionListener{
 		}
 		if (r2.isSelected()) {
 			JOptionPane.showMessageDialog(this, "You are female");
-		} 
-		 if (!(r1.isSelected() && r2.isSelected())) {
-			JOptionPane.showMessageDialog(this, "make choice");
 		}
+
 	}
+
 	public static void main(String[] args) {
 		new Jradiobutton();
 	}

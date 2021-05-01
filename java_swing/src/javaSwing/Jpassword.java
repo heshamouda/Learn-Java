003b56e09c739e5098729d9c8f8b329dd0814a1d
Hesham.Ouda <hesham.ouda@students.fhnw.ch>
2021-05-01 15:40:28 +0200

    delete

--- java_swing/src/javaSwing/Jpassword.java
+++ java_swing/src/javaSwing/Jpassword.java
@@ -1,27 +1,26 @@
 package javaSwing;
 
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 
 public class Jpassword {
 
 	public static void main(String[] args) {
-	 JPasswordField pw = new JPasswordField();
-	 JFrame f = new JFrame("Password");
-	 f.setSize(400,400);
-	 f.setLayout(null);
-	 
-	 pw.setBounds(120, 50, 140, 20);
-	 
-	 JLabel lb = new JLabel("Enter Password");
-	 lb.setBounds(10, 50, 100, 20);
-	 
-	 f.add(lb);
-	 f.add(pw);
-	 f.setVisible(true);
-	 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 
+		JPasswordField pw = new JPasswordField();
+		JFrame f = new JFrame("Password");
+		f.setSize(400, 400);
+		f.setLayout(null);
+
+		pw.setBounds(120, 50, 140, 20);
+
+		JLabel lb = new JLabel("Enter Password");
+		lb.setBounds(10, 50, 100, 20);
+
+		f.add(lb);
+		f.add(pw);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	}
 
 }
